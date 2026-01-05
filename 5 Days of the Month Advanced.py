ebccc0e4e8545a8e58bbd7d0ca8438f616e0874b
Amir <amirtanael3@gmail.com>
2026-01-05 16:05:08 +0000

    Excersise: 5 Days of the Month and advanced (Done)

--- 5 Days of the Month Advanced.py
+++ 5 Days of the Month Advanced.py
@@ -1,3 +1,9 @@
+leap_year = False
+
+year = int(input("Enter the year: "))
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    leap_year = True
+
 months = {
     1: ("January", 31),
     2: ("February", 28),
@@ -12,9 +18,15 @@ months = {
     11: ("November", 30),
     12: ("December", 31)
 }
+if leap_year:
+    months[2] = ("February", 29)
 input_month = int(input("Enter the month number (1-12): "))
 if input_month in months:
     month_name, days = months[input_month]
-    print(f"Month {input_month} has {days} days and is {month_name}.")
+    print(f"Month {input_month} has {days} days and is {month_name} and the year {year} is", end=' ')
+    if leap_year:
+        print("a leap year.")
+    else:
+        print("not a leap year.")
 else:
     print("Invalid month number. Please enter a number between 1 and 12.")
